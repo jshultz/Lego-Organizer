fef6a9224ac94740004082a9a9813331dab69959
Jason Shultz <jasshultz@gmail.com>
2015-12-13 14:57:53 -0700

    part list is showing now

--- Lego_Organizer/LegoSetViewController.swift
+++ Lego_Organizer/LegoSetViewController.swift
@@ -13,11 +13,43 @@ import Alamofire
 
 class LegoSetViewController: UIViewController {
     
-    var setId:String? = nil
+
+    @IBOutlet weak var setNumberLabel: UILabel!
+    
+    @IBOutlet weak var setNameLabel: UILabel!
+    
+    @IBOutlet weak var setDescriptionLabel: UILabel!
+    
+    @IBOutlet weak var setImage: UIImageView!
+    
+    
+    var setId:JSON = nil
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+//        print("setId: ", self.setId)
+        
+        self.setNumberLabel.text = String(UTF8String: self.setId["set_id"].string!)!
+        
+        self.setNameLabel.text = String(UTF8String: self.setId["descr"].string!)!
+        
+        let pieces:String = String(UTF8String: self.setId["pieces"].string!)!
+        
+        let year:String = String(UTF8String: self.setId["year"].string!)!
+        
+        self.setDescriptionLabel.text = "Set consists of \(pieces) pieces and was first produced in \(year)."
+        
+        let imageView = setImage as UIImageView
+        
+        let img_url = String(UTF8String: self.setId["img_sm"].string!)!
+        
+        if let url = NSURL(string: "\(img_url)") {
+            if let data = NSData(contentsOfURL: url) {
+                imageView.image = UIImage(data: data)
+            }
+        }
+        
         // Do any additional setup after loading the view.
     }
 
@@ -27,14 +59,27 @@ class LegoSetViewController: UIViewController {
     }
     
 
-    /*
+
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
+        if segue.identifier == "newPlace" {
+            
+        } else if segue.identifier == "showSetsPartsList" {
+            
+            let PartsController:SetsPartsListTableViewController = segue.destinationViewController as! SetsPartsListTableViewController
+            
+            //            print("activeSet: ", self.datas[activeSet])
+            
+            let setId = self.setId
+            
+            PartsController.setId = setId
+        }
+        
     }
-    */
+
 
 }
--- Lego_Organizer/SetsPartsListTableViewController.swift
+++ Lego_Organizer/SetsPartsListTableViewController.swift
@@ -1,8 +1,8 @@
 //
-//  RootTableViewController.swift
+//  SetsPartsListTableViewController.swift
 //  Lego_Organizer
 //
-//  Created by Jason Shultz on 12/6/15.
+//  Created by Jason Shultz on 12/13/15.
 //  Copyright © 2015 HashRocket. All rights reserved.
 //
 
@@ -11,26 +11,55 @@ import RealmSwift
 import SwiftyJSON
 import Alamofire
 
-class RootTableViewController: UITableViewController {
+class SetsPartsListTableViewController: UITableViewController {
     
-    let realm = try! Realm()
-    
-    var profile:Profile? = nil
-    
-    var apiKey:String = ""
-    
-    var userSets:NSArray = []
-    
-    var activeSet = -1
-    
-//    var datas: [JSON] = []
+    var setId:JSON = nil
     var datas: JSON = []
+    let realm = try! Realm()
+    var profile:Profile? = nil
     
     @IBOutlet var legoTable: UITableView!
     
+    
+    func setupUI() {
+        self.title = "Lego Organizer"
+        
+        if let profile = realm.objects(Profile).first {
+            self.profile = profile
+        }
+        
+        if profile?.userHash != "" {
+            
+            
+            Alamofire.request(.GET, "https://rebrickable.com/api/get_set_parts", parameters: ["key": "9BUbjlV9IF", "set" : String(UTF8String: self.setId["set_id"].string!)!, "format": "json"]).validate().responseJSON { response in
+                switch response.result {
+                case .Success:
+                    if response.result.value != nil {
+                        var jsonObj = JSON(response.result.value!)
+//                        print("jsonObj: ", jsonObj)
+                        
+                        if let data:JSON = JSON(jsonObj[0]["parts"].arrayValue) {
+                            self.datas = data
+//                            print("jsonObj: ", jsonObj)
+//                            print("datas: ", self.datas)
+//                            print("data: ", response.result.value)
+                            self.legoTable.reloadData()
+                        }
+                    }
+                    
+                case .Failure(let error):
+                    print(error)
+                }
+            }
+            
+        }
+        
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setupUI()
 
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
@@ -55,51 +84,11 @@ class RootTableViewController: UITableViewController {
         // #warning Incomplete implementation, return the number of rows
         return datas.count
     }
-    
-    override func viewWillAppear(animated: Bool) {
-        setupUI()
-    }
-    
-    func getUserSets(userHash:String, completionHandler: (JSON?) -> ()) -> () {
-        
-    }
-    
-    func setupUI() {
-        self.title = "Lego Organizer"
-        
-        if let profile = realm.objects(Profile).first {
-            self.profile = profile
-        }
-        
-        if profile?.userHash != "" {
-            
-            
-            Alamofire.request(.GET, "https://rebrickable.com/api/get_user_sets", parameters: ["key": "9BUbjlV9IF", "hash" : (profile?.userHash)!, "format": "json"]).validate().responseJSON { response in
-                switch response.result {
-                case .Success:
-                    if response.result.value != nil {
-                        var jsonObj = JSON(response.result.value!)
-//                        print("jsonObj: ", jsonObj)
-                        
-                        if let data:JSON = JSON(jsonObj[0]["sets"].arrayValue) {
-                            self.datas = data
-//                            print("jsonObj: ", jsonObj)
-//                            print("datas: ", self.datas)
-//                            print("data: ", response.result.value)
-                            self.legoTable.reloadData()
-                        }
-                    }
-                    
-                case .Failure(let error):
-                    print(error)
-                }
-            }
-            
-        }
-        
-    }
-    
+
+
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+//        print("datas[indexPath.row]: ", datas[indexPath.row])
         
         let cell = tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath)
         
@@ -109,23 +98,23 @@ class RootTableViewController: UITableViewController {
         
         let object = datas[indexPath.row]
         
-        if let userName = object["set_id"].string {
-            let img_url = String(UTF8String: object["img_sm"].string!)!
+        if let userName = object["part_id"].string {
+            let img_url = String(UTF8String: object["part_img_url"].string!)!
 //            print("userName", userName)
-            cell.textLabel?.text = userName
+            cell.textLabel?.text = userName + " " + String(UTF8String: object["part_name"].string!)!
             
             if let url = NSURL(string: "\(img_url)") {
                 if let data = NSData(contentsOfURL: url) {
                     imageView.image = UIImage(data: data)
-                }        
+                }
             }
-            subTitle.text = object["descr"].string
+            subTitle.text = object["color_name"].string
         }
 //        print("object: ", object)
         
         return cell
     }
-    
+
 
     /*
     // Override to support conditional editing of the table view.
@@ -135,7 +124,7 @@ class RootTableViewController: UITableViewController {
     }
     */
 
-
+    /*
     // Override to support editing the table view.
     override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
         if editingStyle == .Delete {
@@ -145,7 +134,7 @@ class RootTableViewController: UITableViewController {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }    
     }
-
+    */
 
     /*
     // Override to support rearranging the table view.
@@ -161,36 +150,15 @@ class RootTableViewController: UITableViewController {
         return true
     }
     */
-    
-    override func tableView(tableView: UITableView, willSelectRowAtIndexPath indexPath: NSIndexPath) -> NSIndexPath? {
-//        print("indexPath: ", indexPath)
-        activeSet = indexPath.row
-        return indexPath
-    }
 
-
+    /*
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
-        
-        if segue.identifier == "newPlace" {
-            
-        } else if segue.identifier == "showLegoSet" {
-            
-            let legoSetController:LegoSetViewController = segue.destinationViewController as! LegoSetViewController
-            
-//            print("activeSet: ", self.datas[activeSet])
-            
-            let setId = self.datas[activeSet]
-            
-            legoSetController.setId = setId
-        }
-        
-        self.title = ""
     }
-
+    */
 
 }
