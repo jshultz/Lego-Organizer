63ca1f0f88db5be87c75080aca6978a4689c9922
Jason Shultz <jasshultz@gmail.com>
2015-12-13 19:45:24 -0700

    creating a set

---
+++ Lego_Organizer/AddLegoSetViewController.swift
@@ -0,0 +1,67 @@
+//
+//  AddLegoSetViewController.swift
+//  Lego_Organizer
+//
+//  Created by Jason Shultz on 12/13/15.
+//  Copyright © 2015 HashRocket. All rights reserved.
+//
+
+import UIKit
+import RealmSwift
+import Alamofire
+
+class AddLegoSetViewController: UIViewController {
+    
+    @IBOutlet weak var setDescription: UITextField!
+    
+    @IBAction func saveSet(sender: AnyObject) {
+        
+        createSet("test set", password: "password") { (accessKey) -> () in
+            if accessKey != nil {
+                print("accessKey: ", accessKey)
+            }
+        }
+        
+    
+    }
+    
+    func createSet(username:String, password:String, completionHandler: (String?) -> ()) -> () {
+        Alamofire.request(.POST, "https://rebrickable.com/api/get_user_hash",
+            parameters: [
+                "key": "9BUbjlV9IF",
+                "hash" : "8d6678c55a5a93393b19fd2f38e44ed1",
+                "desc": "some stuff here",
+                "format": "json",
+                "type": "1"
+            ]
+            )
+            .responseString { response in
+                
+                completionHandler(response.result.value)
+                
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
